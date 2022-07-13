c261df86c6e01feaca76065db4ecdad37ed65939
Zohidbek Mengliboyev <uzrivojcoder@gmail.com>
2022-07-13 17:36:38 +0500

    configuring app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TxteditorComponent } from "./txteditor/txteditor.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "txteditor", pathMatch: "full"},
+  {path: "txteditor", component: TxteditorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
